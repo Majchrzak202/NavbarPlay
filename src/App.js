71bf762aedf52ccb7dc5bbe3502449f4fea81601
MarcinMaster <m.luba505@gmail.com>
2022-05-24 08:33:49 +0200

    Adding a Router to app and specyfing how it will work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import Navbar from "./Components/Navbar/Index";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import PageNotFound from "./Pages/PageNotFound";
+import Navbar from "./Components/Navbar";
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     <Navbar/> 
-    </div>
+    <Router>
+      <Navbar />
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
